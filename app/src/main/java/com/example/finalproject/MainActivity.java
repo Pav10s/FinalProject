f003f60dce1602cf049109d46442ffbd0927f485
Pav10s <pavithraos123@gmail.com>
2023-03-01 05:10:59 +0530

    Added option for different phones, Recycler View

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,47 +1,83 @@
 package com.example.finalproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
+public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
+    //private Button button1;
+    //private Button button2;
+    //private Button button3;
+    //private Button button4;
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    TextView phName;
+    String userID,PhName;
+
+    ArrayList<RecModel> recModels = new ArrayList<>();
+
+    int[] recImages = {R.drawable.mock,R.drawable.mock1,
+            R.drawable.mock,R.drawable.mock1,
+            R.drawable.mock,R.drawable.mock1,
+            R.drawable.mock,R.drawable.mock1,
+            R.drawable.mock,R.drawable.mock1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        phName = findViewById(R.id.PhName);
+
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        DocumentReference documentReference = fStore.collection("users").document(userID);
 
-        button1 = findViewById(R.id.phone);
-        button2 = findViewById(R.id.message);
-        button3 = findViewById(R.id.whatsapp);
-        button4 = findViewById(R.id.gpay);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                phName.setText(value.getString("phName"));
+                PhName = value.getString("phName");
+            }
+        });
 
-        button1.setOnClickListener(v -> startActivity(new Intent(this,Phone.class)));
+        RecyclerView recyclerView = findViewById(R.id.mRecycler);
+        setRecModels();
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,recModels, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        button2.setOnClickListener(v -> startActivity(new Intent(this,Message.class)));
+    }
 
-        button3.setOnClickListener(v -> startActivity(new Intent(this,Whatsapp.class)));
-
-        button4.setOnClickListener(v -> startActivity(new Intent(this,GooglePay.class)));
-
+    private void setRecModels() {
+        String[] recModelNames = getResources().getStringArray(R.array.recText);
 
 
+        for (int i=0;i<recModelNames.length;i++) {
+            recModels.add(new RecModel(recModelNames[i],recImages[i]));
+        }
     }
 
     @Override
@@ -58,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.about:
                 startActivity(new Intent(MainActivity.this, About.class));
+                Toast.makeText(this, PhName,Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.donate:
@@ -79,5 +116,53 @@ public class MainActivity extends AppCompatActivity {
         finishAffinity();
         System.exit(0);
 
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        if (PhName == "Samsung") {
+            if (position == 0)
+                startActivity(new Intent(this, Phone.class));
+            else if (position == 1)
+                startActivity(new Intent(this, Message.class));
+            else if (position == 2)
+                startActivity(new Intent(this, Phone.class));
+            else if (position == 3)
+                startActivity(new Intent(this, Message.class));
+            else if (position == 4)
+                startActivity(new Intent(this, Phone.class));
+            else if (position == 5)
+                startActivity(new Intent(this, Message.class));
+            else if (position == 6)
+                startActivity(new Intent(this, Phone.class));
+            else if (position == 7)
+                startActivity(new Intent(this, Message.class));
+            else if (position == 8)
+                startActivity(new Intent(this, Phone.class));
+            else if (position == 9)
+                startActivity(new Intent(this, Message.class));
+        }
+        else {
+            if (position == 0)
+                startActivity(new Intent(this, Whatsapp.class));
+            else if (position == 1)
+                startActivity(new Intent(this, GooglePay.class));
+            else if (position == 2)
+                startActivity(new Intent(this, Whatsapp.class));
+            else if (position == 3)
+                startActivity(new Intent(this, GooglePay.class));
+            else if (position == 4)
+                startActivity(new Intent(this, Whatsapp.class));
+            else if (position == 5)
+                startActivity(new Intent(this, GooglePay.class));
+            else if (position == 6)
+                startActivity(new Intent(this, Whatsapp.class));
+            else if (position == 7)
+                startActivity(new Intent(this, GooglePay.class));
+            else if (position == 8)
+                startActivity(new Intent(this, Whatsapp.class));
+            else if (position == 9)
+                startActivity(new Intent(this, GooglePay.class));
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/SignUp.java
+++ app/src/main/java/com/example/finalproject/SignUp.java
@@ -4,28 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
     private Button button1;
 
     EditText r_email;
     EditText pass;
     Button log;
 
+    String[] phones = { "Select Phone", "Samsung", "Realme"}; //To add the list of phones
+    String userID;
+
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+
 
 
     @Override
@@ -37,18 +53,36 @@ public class SignUp extends AppCompatActivity {
         pass = findViewById(R.id.Password1);
         log = findViewById(R.id.register1);
 
+        Spinner spin = findViewById(R.id.Spin);
+
+        // Create the instance of ArrayAdapter having the list of courses
+        ArrayAdapter ad
+                = new ArrayAdapter(this, android.R.layout.simple_spinner_item, phones);
+
+        // set simple layout resource file for each item of spinner
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the Spinner which binds data to spinner
+        spin.setAdapter(ad);
+
         button1 = findViewById(R.id.login);
         button1.setOnClickListener(v -> Login());
 
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        log.setOnClickListener(view -> createUser());
+        log.setOnClickListener(view -> createUser(spin));
 
     }
+
+
     
-    private void createUser(){
+    private void createUser(Spinner spin){
         String email = r_email.getText().toString();
         String password = pass.getText().toString();
+        String selectedItem = spin.getSelectedItem().toString();
 
         if(TextUtils.isEmpty(email)){
             r_email.setError("Email cannot be empty");
@@ -58,12 +92,27 @@ public class SignUp extends AppCompatActivity {
             pass.setError("Password cannot be empty");
             pass.requestFocus();
         }
+
+        else if(selectedItem == "Select Phone"){
+            Toast.makeText(this,"Phone must be selected",Toast.LENGTH_LONG).show();
+        }
+
         else{
             mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
-                        Toast.makeText(SignUp.this,"User Registered",Toast.LENGTH_LONG).show();
+                        Toast.makeText(SignUp.this,selectedItem,Toast.LENGTH_LONG).show();
+                        userID = mAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("phName",selectedItem);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG,"Created"+userID);
+                            }
+                        });
                         startActivity(new Intent(SignUp.this,Login.class));
                     }else{
                         Toast.makeText(SignUp.this,"Registration Unsuccessful " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
