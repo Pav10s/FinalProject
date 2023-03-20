8e598e01e56a96fe32e389361c1e7c635b17e49a
Pav10s <pavithraos123@gmail.com>
2023-03-20 21:27:32 +0530

    Added Payment, Some changes in color theme

--- app/src/main/java/com/example/finalproject/Donation.java
+++ app/src/main/java/com/example/finalproject/Donation.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,13 +22,14 @@ public class Donation extends AppCompatActivity implements PaymentResultListener
     FirebaseAuth mAuth;
     FirebaseUser user;
     String email;
-    private EditText amountEdt;
+    private EditText amountEdt, ph_number;
     private Button payBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donations);
+        getSupportActionBar().hide();
 
         mAuth = FirebaseAuth.getInstance();
         user =mAuth.getCurrentUser();
@@ -35,6 +37,7 @@ public class Donation extends AppCompatActivity implements PaymentResultListener
 
         // initializing all our variables.
         amountEdt = findViewById(R.id.idEdtAmount);
+        ph_number = findViewById(R.id.ph_number);
         payBtn = findViewById(R.id.idBtnPay);
 
         // adding on click listener to our button.
@@ -44,46 +47,54 @@ public class Donation extends AppCompatActivity implements PaymentResultListener
                 // on below line we are getting
                 // amount that is entered by user.
                 String samount = amountEdt.getText().toString();
+                String phnumber = ph_number.getText().toString();
 
-                // rounding off the amount.
-                int amount = Math.round(Float.parseFloat(samount) * 100);
-
-                // initialize Razorpay account.
-                Checkout checkout = new Checkout();
-
-                // set your id as below
-                checkout.setKeyID("rzp_test_bYh9kKGWAILAhv");
+                if (TextUtils.isEmpty(samount) || TextUtils.isEmpty(phnumber))
+                    amountEdt.setError("This field cannot be empty");
 
 
+                else {
 
-                // initialize json object
-                JSONObject object = new JSONObject();
-                try {
-                    // to put name
-                    object.put("name", "Grand Phone");
+                    // rounding off the amount.
+                    int amount = Math.round(Float.parseFloat(samount) * 100);
 
-                    // put description
-                    object.put("description", "Test payment");
+                    // initialize Razorpay account.
+                    Checkout checkout = new Checkout();
 
-                    // to set theme color
-                    object.put("theme.color", "#25383C");
+                    // set your id as below
+                    checkout.setKeyID("rzp_test_bYh9kKGWAILAhv");
 
-                    // put the currency
-                    object.put("currency", "INR");
 
-                    // put amount
-                    object.put("amount", amount);
 
-                    // put mobile number
-                    object.put("prefill.contact", "7306158986");
+                    // initialize json object
+                    JSONObject object = new JSONObject();
+                    try {
+                        // to put name
+                        object.put("name", "Grand Phone");
 
-                    // put email
-                    object.put("prefill.email", email);
+                        // put description
+                        object.put("description", "Test payment");
 
-                    // open razorpay to checkout activity
-                    checkout.open(Donation.this, object);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                        // to set theme color
+                        object.put("theme.color", "#25383C");
+
+                        // put the currency
+                        object.put("currency", "INR");
+
+                        // put amount
+                        object.put("amount", amount);
+
+                        // put mobile number
+                        object.put("prefill.contact", phnumber);
+
+                        // put email
+                        object.put("prefill.email", email);
+
+                        // open razorpay to checkout activity
+                        checkout.open(Donation.this, object);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
